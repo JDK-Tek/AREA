099827312cbbe40a6a2c8d56da753ffb855abdbd
John DE KETTELBUTTER <john.de-kettelbutter@epitech.eu>
2024-11-19 17:33:27 +0100

    feat[#3]: grey background + 2 text fields for email and passwd using materialui

--- benchmark/web/react/src/App.js
+++ benchmark/web/react/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import TextField from '@mui/material/TextField'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='TextField'>
+          <TextField label="E-mail"/>
+          <TextField label="Password"/>
+        </div>
       </header>
     </div>
   );
