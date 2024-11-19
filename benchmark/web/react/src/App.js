f55a6a6b10bdd90f805000db2cbfa65ab7ea860c
John DE KETTELBUTTER <john.de-kettelbutter@epitech.eu>
2024-11-19 22:58:49 +0100

    feat[#3]: loading time using PerformanceNavigationTiming API + fix some size problems

--- benchmark/web/react/src/App.js
+++ benchmark/web/react/src/App.js
@@ -1,16 +1,28 @@
 import * as React from 'react';
-import TextField from '@mui/material/TextField'
+import { useState, useEffect } from 'react';
+import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button'
+import Button from '@mui/material/Button';
 
 import './App.css';
 
 function App() {
+  const [loadTime, setLoadTime] = useState(0);
+
+  useEffect(() => {
+    const [navigation] = performance.getEntriesByType('navigation');
+    if (navigation) {
+      const loadTime = navigation.domContentLoadedEventEnd - navigation.startTime;
+      setLoadTime(loadTime);
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Benchmark AREA - ReactJS</p>
         <p>Elise PIPET - Grégoire LANTIM - Paul PARISOT - Esteban MARQUES - John DE KETTELBUTTER</p>
+        <p>Temps de chargement : {loadTime} ms</p>
       </header>
       <div className='TextField'>
         <Stack spacing={2} direction="column" className='TextField'>
