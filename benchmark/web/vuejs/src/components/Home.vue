6528bf655d1801eb31336bebc40d71ab8408c22a
John DE KETTELBUTTER <john.de-kettelbutter@epitech.eu>
2024-11-23 00:57:02 +0100

    feat[#4]: dropdown button for home page + display of web images + import of all others images

--- benchmark/web/vuejs/src/components/Home.vue
+++ benchmark/web/vuejs/src/components/Home.vue
@@ -1,11 +1,82 @@
 <template>
-    <div>
-      <p>Test</p>
+    <div class='Menu'>
+        <Menu/>
+    </div>
+    <div class="div-techno">
+        <img :src="reactJSLogo" class="img-techno" alt="React"/>
+        <img :src="HTMLLogo" class="img-techno" alt="React"/>
+        <img :src="VueJSLogo" class="img-techno" alt="React"/>
     </div>
   </template>
-  
-  <script>
-  export default {
-    name: 'Home'
-  };
+
+<style>
+.Menu {
+    text-align: center;
+    margin-top: 1.3rem;
+}
+
+.div-techno {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 20px;
+}
+
+.img-techno {
+  width: 20vh;
+  height: 20vh;
+  background-color: white;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  object-fit: cover;
+  display: block;
+  margin: auto;
+  padding: 20px;
+}
+</style>
+
+<script>
+    import reactJS from '@/assets/web/reactjs.png';
+    import HTML from '@/assets/web/html.png';
+    import VueJS from '@/assets/web/vuejs.png';
+
+    import Django from '@/assets/backend/django.png';
+    import GO from '@/assets/backend/go.png';
+    import NestJS from '@/assets/backend/nestjs.jpg';
+
+    import MariaDB from '@/assets/database/mariadb.jpeg';
+    import MySQL from '@/assets/database/mysql.png';
+    import PostGres from '@/assets/database/postgres.png';
+    import SQLite from '@/assets/database/sqlite.png';
+
+    import Flutter from '@/assets/mobile/flutter.png';
+    import Kotlin from '@/assets/mobile/kotline.png';
+    import ReactNative from '@/assets/mobile/reactnative.png';
+    
+    import Menu from './Menu.vue'
+
+    export default {
+        name: 'Home',
+        components: {
+            Menu
+        },
+        data() {
+            return {
+                reactJSLogo: reactJS,
+                HTMLLogo: HTML,
+                VueJSLogo: VueJS,
+                DjangoLogo: Django,
+                GOLogo: GO,
+                NestJSLogo: NestJS,
+                MariaDBLogo: MariaDB,
+                MySQLLogo: MySQL,
+                PostGresLogo: PostGres,
+                SQLiteLogo: SQLite,
+                FlutterLogo: Flutter,
+                KotlinLogo: Kotlin,
+                ReactNativeLogo: ReactNative
+            };
+        }
+    };
   </script>
