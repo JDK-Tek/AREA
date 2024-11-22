5c4acfad6a5a15d452ccfa9e4e7a44aa9b067298
John DE KETTELBUTTER <john.de-kettelbutter@epitech.eu>
2024-11-23 00:20:59 +0100

    feat[#4]: confirm button + use of vue-router to go to home page when click

--- benchmark/web/vuejs/src/App.vue
+++ benchmark/web/vuejs/src/App.vue
@@ -1,5 +1,31 @@
+<template>
+  <div class="App">
+    <header class="App-header">
+      <p>Benchmark AREA - VueJS</p>
+      <p>Elise PIPET - Grégoire LANTIM - Paul PARISOT - Esteban MARQUES - John DE KETTELBUTTER</p>
+      <p>Temps de chargement : {{ loadTime }} ms</p>
+    </header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loadTime: 0
+    };
+  },
+  mounted() {
+    const performance = window.performance;
+    const loadTime = performance.now();
+    this.loadTime = loadTime.toFixed(2);
+  }
+}
+</script>
+
 <style>
-.main-class {
+.App {
   text-align: center;
   background-color: #f1f1f1;
   min-height: 100vh;
@@ -14,49 +40,4 @@
   gap: 0.4rem;
 }
 
-.TextField {
-  display: flex;
-  /* min-height: 30vh; */
-  max-width: 20vw;
-  flex-direction: column;
-  justify-content: center;
-  /* gap: 10px; */
-  margin: 0 auto;
-  margin-top: 5vh;
-}
 </style>
-
-
-<template>
-  <main class="main-class">
-    <header class="App-header">
-      <p>Benchmark AREA - VueJS</p>
-      <p>Elise PIPET - Grégoire LANTIM - Paul PARISOT - Esteban MARQUES - John DE KETTELBUTTER</p>
-      <p>Temps de chargement : {{ loadTime }} ms</p>
-    </header>
-    <div class="TextField">
-      <TextField label="Email"></TextField>
-      <TextField label="Password"></TextField>
-    </div>
-  </main>
-</template>
-
-<script>
-import TextField from './TextField.vue';
-
-export default {
-  components: {
-    TextField
-  },
-  data() {
-    return {
-      loadTime: 0
-    };
-  },
-  mounted() {
-    const performance = window.performance;
-    const loadTime = performance.now();
-    this.loadTime = loadTime.toFixed(2); // Arrondir à 2 décimales
-  }
-}
-</script>
