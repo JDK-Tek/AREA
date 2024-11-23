009e32e17ada92ecbbe7c0855e836feb6e61758b
Gregoire lan tim <gregoire.lan-tim@epitech.eu>
2024-11-23 15:55:06 +0100

    feat(#7): add the dropButton with the images

--- benchmark/mobile/benshmark_flutter/lib/pages/home_page.dart
+++ benchmark/mobile/benshmark_flutter/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:benshmark/tools/screen_scale.dart';
+//import 'package:benshmark/tools/space.dart';
 
 const ColorScheme mainColorScheme = ColorScheme(
   brightness: Brightness.light,
@@ -24,6 +25,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late String button;
+  List<String> _list = ["test1", "test2"];
   String i = "";
   void test(int index) {
     setState(() {});
@@ -31,23 +34,72 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
+    button = _list[0];
     super.initState();
   }
 
   PreferredSizeWidget myOwnAppbar(BuildContext context) {
     return PreferredSize(
-      preferredSize: screenScale(context, .1),
-      child: const Text("coucou")
-    );
+        preferredSize: screenScale(context, .1), child: const Text("coucou"));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: myOwnAppbar(context),
-        body: Container(
-          color: Colors.white,
-        ),
+      appBar: myOwnAppbar(context),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              //const SpaceW(width: 10,),
+              Center(
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          "https://blog.realogs.in/content/images/2021/09/kotlin.png"),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              ),
+              //const SpaceW(width: 250,),
+              Center(
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          "https://i2.wp.com/softwareengineeringdaily.com/wp-content/uploads/2018/10/flutter.jpg?fit=1570,1500&ssl=1"),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          DropdownButton<String>(
+            value: button,
+            icon: const Icon(Icons.arrow_downward),
+            elevation: 16,
+            onChanged: (val) {
+              setState(() {
+                button = val!;
+              });
+            },
+            items: _list.map((val) {
+              return DropdownMenuItem<String>(
+                value: val,
+                child: Text(val),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
     );
   }
 }
