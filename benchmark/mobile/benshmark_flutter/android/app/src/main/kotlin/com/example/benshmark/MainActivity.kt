e061cad83523d115a0bb610c1ce86ee75947d62d
Gregoire lan tim <gregoire.lan-tim@epitech.eu>
2024-11-26 23:18:10 +0100

    fix(#7): delete tempo files

--- benchmark/mobile/benshmark_flutter/android/app/src/main/kotlin/com/example/benshmark/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.benshmark
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- benchmark/mobile/benshmark_flutter/lib/tools/screen_scale.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-Size screenScale(BuildContext context, double scale) =>
-    MediaQuery.of(context).size * scale;
