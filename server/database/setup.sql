6b05c1be8e2bbb484141f950c517a105f1c94294
Paul PARISOT <paul.parisot@epitech.eu>
2024-11-30 11:49:56 +0100

    feat(#40): database creation for testing

--- server/database/setup.sql
+++ server/database/setup.sql
@@ -1,7 +1,13 @@
-drop database if exists area_database;
-drop user if exists area_user;
+-- drop database if exists area_database;
+-- drop user if exists area_user;
 
-create database area_database;
-create user area_user with encrypted password 'password';
-grant select, insert, update, delete on all tables in schema public to area_user;
+-- create database area_database;
+-- create user area_user with encrypted password 'password';
+-- grant select, insert, update, delete on all tables in schema public to area_user;
 
+-- Create the users table if it does not already exist
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
+);
