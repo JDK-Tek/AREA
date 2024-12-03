4324fd39937337dd54cf6a9ace85cef9cac4b591
Esteban <esteban.marques@epitech.eu>
2024-12-03 17:15:00 +0100

    feat[#48]: implement `Service` and `ServiceKit` component

--- web/src/components/AppletKit.js
+++ web/src/components/AppletKit.js
@@ -6,13 +6,17 @@
 */
 import Applet from "./Applet";
 
-export default function AppletKit({ title, applets }) {
+export default function AppletKit({ title, applets, color = "text-black" }) {
+
     return (
         <div className="text-center p-5">
-            <label className="font-spartan text-[35px] font-bold">{title}</label>
+            <label className={`font-spartan ${color} text-[35px] font-bold`}>{title}</label>
             <div className="mt-5 flex flex-wrap justify-center gap-7 p-5">
                 {applets.map((applet, index) => (
-                    <Applet key={index} applet={applet} />
+                    <Applet
+                        key={index}
+                        applet={applet}
+                    />
                 ))}
             </div>
         </div>
