b1b491c383586f9bd55ec834fe37ac62335cff93
Esteban <esteban.marques@epitech.eu>
2024-12-03 17:15:00 +0100

    feat[#48]: implement `Service` and `ServiceKit` component

--- web/src/App.js
+++ web/src/App.js
@@ -6,13 +6,27 @@
 */
 
 import AppletData from "./data/AppletData";
+import ServiceData from "./data/ServiceData";
+
 import AppletKit from "./components/AppletKit";
+import ServiceKit from "./components/ServiceKit";
 
 function App() {
 
   return (
     <div className="App">
-      <AppletKit title={"Get started with any Applet"} applets={AppletData}/>
+      
+      <AppletKit
+        title={"Get started with any Applet"}
+        applets={AppletData}
+      />
+
+      <ServiceKit
+        title={"or choose from 900+ services"}
+        services={ServiceData}
+        color={"text-chartpurple-200"}
+      />
+      
     </div>
   );
 }
