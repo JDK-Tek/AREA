be79bc864226816716f24bf79023fa8de087d1a7
Esteban <esteban.marques@epitech.eu>
2024-12-03 14:42:07 +0100

    build[#48]: add the basical build web react app
    * Adding the web react app source code
    * Implementation of tailwind css in the react app
    * Adding new fonts

---
+++ web/src/App.js
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** AREA
+** File description:
+** App
+*/
+
+// import AppletData from "./data/AppletData";
+// import AppletKit from "./components/AppletKit";
+
+function App() {
+
+  return (
+    <div className="App">
+      {/* <AppletKit title={"Get started with any Applet"} applets={AppletData}/> */}
+    </div>
+  );
+}
+
+export default App;
