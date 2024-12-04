8be82906fe58276b1d3610ab7de0426f86c5e0d0
John DE KETTELBUTTER <john.de-kettelbutter@epitech.eu>
2024-12-04 16:15:35 +0100

    feat(#47): beginning of login page with background gradient + login box with text fields

---
+++ web/src/routes/login/Content.js
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** AREA
+** File description:
+** Content
+*/
+
+import LoginBox from "../../components/LoginBox/LoginBox";
+
+export default function Content() {
+
+    return (
+        <div className="pt-20 pb-14 justify-center flex h-screen">
+            <LoginBox />
+        </div>
+    );
+}
