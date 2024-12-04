fabbe4a5862d908d8c8f6dfa95dfa8ffaf06fa12
John DE KETTELBUTTER <john.de-kettelbutter@epitech.eu>
2024-12-04 18:03:15 +0100

    feat(#47): login box + animated background, still need to implement responsive design

--- web/src/routes/login/Login.js
+++ web/src/routes/login/Login.js
@@ -11,7 +11,7 @@ import Content from './Content';
 export default function Login() {
 
     return (
-        <div className="bg-gradient-to-br from-zinc-900 via-indigo-900 to-violet-900">
+        <div className="bg-gradient-to-br from-zinc-900 via-indigo-900 to-violet-900 animate-gradient bg-300%">
             <Content />
             <Footer />
         </div>
