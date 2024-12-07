4695f60aa992a6fea1ce8afd5f7ff90129f35cc6
Paul PARISOT <paul.parisot@epitech.eu>
2024-12-07 22:40:35 +0100

    feat(#61): get the correct timestamp
    * it adds the good amount
    * so i get the good timestamp

--- server/services/time/main.go
+++ server/services/time/main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"database/sql"
+	"errors"
+	"time"
+
 	// "bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+
 	// "net/url"
 	"os"
 	// "strconv"
-	// "time"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
@@ -19,13 +22,14 @@ import (
 
 const API = "https://tools.aimylogic.com/api/now?tz=Europe/Paris"
 
-type Time struct {
+type Spices struct {
 	HowMuch int `json:"howmuch"`
 	Unit string `json:"unit"`
 }
 
 type Content struct {
-	Spices Time `json:"spices"`
+	Spices Spices `json:"spices"`
+	BridgeId int `json:"bridge"`
 }
 
 type Response struct {
@@ -37,12 +41,13 @@ func makeAnError(w http.ResponseWriter, err error, status int) {
 	fmt.Fprintf(w, "{ \"error\": \"%s\" }\n", err.Error())
 }
 
-func timeIn(w http.ResponseWriter, req *http.Request, db *sql.DB) {
-	var userContent Content
-	var timeRep Response
+func getTimeNow(w http.ResponseWriter, req *http.Request) (c Content, n int64, err error) {
+	var r Response
 
+	c.BridgeId = -1
+	c.Spices.HowMuch = -1
 	decoder := json.NewDecoder(req.Body)
-	err := decoder.Decode(&userContent)
+	err = decoder.Decode(&c)
 	if err != nil {
 		makeAnError(w, err, http.StatusBadRequest)
 		return
@@ -58,13 +63,56 @@ func timeIn(w http.ResponseWriter, req *http.Request, db *sql.DB) {
 		makeAnError(w, err, http.StatusBadGateway)
 		return
 	}
-	err = json.Unmarshal(body, &timeRep)
+	err = json.Unmarshal(body, &r)
 	if err != nil {
 		makeAnError(w, err, http.StatusBadGateway)
 		return
 	}
+	if c.BridgeId == -1 || c.Spices.HowMuch == -1 {
+		err = errors.New("invalid parsing")
+		makeAnError(w, err, http.StatusBadRequest)
+		return
+	}
+	n = r.Timestamp
+	return
+}
+
+func spices2Seconds(spices Spices) int64 {
+	var association = map[string]int{
+		"weeks": 7 * 24 * 3600,
+		"days": 24 * 3600,
+		"hours": 3600,
+		"minutes": 60,
+		"seconds": 1,
+	}
+	return int64(spices.HowMuch * association[spices.Unit])
+}
+
+func timeIn(w http.ResponseWriter, req *http.Request, db *sql.DB) {
+	var maybeid int
+
+	content, ms, err := getTimeNow(w, req)
+	if err != nil {
+		return
+	}
+	err = db.QueryRow("select id from micro_time where bridgeid = $1", content.BridgeId).Scan(&maybeid)
+	if err == nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "{ \"error\": \"it already exists\" }\n")
+		return
+	}
+	secs := ms / 1000
+	nsecs := (ms % 1000) * 1e6
+	secs += spices2Seconds(content.Spices)
+	timestamp := time.Unix(secs, nsecs)
+	_, err = db.Exec("insert into micro_time (bridgeid, triggers) values ($1, $2)", content.BridgeId, timestamp)
+	fmt.Println("hello man !")
+	if err != nil {
+		makeAnError(w, err, http.StatusInternalServerError)
+		return
+	}
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "hello %v", timeRep.Timestamp)
+	fmt.Fprintf(w, "hello %v", timestamp)
 }
 
 func connectToDatabase() (*sql.DB, error) {
@@ -88,13 +136,18 @@ func connectToDatabase() (*sql.DB, error) {
 	if dbPort == "" {
 		log.Fatal("DB_PORT not found")
 	}
+	// connectStr := fmt.Sprintf(
+	// 	"postgresql://%s:%s@%s:%s/%s?sslmode=disable",
+	// 	dbUser,
+	// 	dbPassword,
+	// 	dbHost,
+	// 	dbPort,
+	// 	dbName,
+	// )
 	connectStr := fmt.Sprintf(
-		"postgresql://%s:%s@%s:%s/%s?sslmode=disable",
+		"postgresql://%s:%s@database:5432/area_database?sslmode=disable",
 		dbUser,
 		dbPassword,
-		dbHost,
-		dbPort,
-		dbName,
 	)
 	return sql.Open("postgres", connectStr)
 }
