9870978c908fe0e6ece4e0cbfdfcae62e0e26037
Esteban <esteban.marques@epitech.eu>
2024-12-07 16:54:18 +0100

    feat[#48]: implement header without design effect background

---
+++ web/src/routes/home/header/Title.js
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** AREA
+** File description:
+** Title
+*/
+
+export default function Title() {
+    return (
+        <div className="max-w-[500px] w-full mx-auto pb-2">
+            <h1 className="text-5xl font-bold font-spartan text-white text-center">AUTOMATION FOR BUISNESS AND HOME</h1>
+            <p className="m-2 text-2xl font-spartan font-bold text-chartpurple-100 text-center">Save time and get more done</p>
+        </div>
+    );
+}
