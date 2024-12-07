52e2071dc0e7dc12e4d28a1ac31b40f0ba500758
John DE KETTELBUTTER <john.de-kettelbutter@epitech.eu>
2024-12-07 14:04:25 +0100

    feat(#47): first version of register, linking with back coming

---
+++ web/src/routes/register/Content.js
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** AREA
+** File description:
+** Content
+*/
+
+import RegisterBox from "../../components/RegisterBox/RegisterBox";
+
+export default function Content() {
+
+    return (
+        <div className="h-screen justify-center items-center flex">
+            <RegisterBox />
+        </div>
+    );
+}
