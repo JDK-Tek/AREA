8db2c147f0555ccd6bb70f0c76bf2a2d9708500c
John DE KETTELBUTTER <john.de-kettelbutter@epitech.eu>
2024-12-07 14:04:25 +0100

    feat(#47): first version of register, linking with back coming

--- web/src/components/RegisterBox/RegisterBox.js
+++ web/src/components/RegisterBox/RegisterBox.js
@@ -2,17 +2,17 @@
 ** EPITECH PROJECT, 2024
 ** AREA
 ** File description:
-** LoginBox
+** RegisterBox
 */
 
-function LoginTexts() {
+function RegisterTexts() {
     return (
         <div className="text-center">
             <p className="text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-spartan font-bold">
-                LOGIN
+                Register
             </p>
             <p className="text-violet-600 font-bold text-xl sm:text-2xl md:text-3xl">
-                Nice to see you again
+                Welcome to AREA !
             </p>
         </div>
     )
@@ -51,19 +51,18 @@ function Button( {text} ) {
     )
 }
 
-export default function LoginBox () {
+export default function RegisterBox () {
     return (
         <div className="bg-gradient-to-b from-zinc-700 to-gray-800 flex flex-col justify-center 
                         w-3/4 sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-2/3 
                         h-4/6 sm:h-3/4 md:h-2/3 lg:h-3/4 rounded-md">
-            <LoginTexts />
+            <RegisterTexts />
             <LoginTextFieldsBox text1="Email" text2="Password" />
             <div className="text-center pt-8 sm:pt-10 text-white text-sm sm:text-base md:text-lg">
-                You don't have an account ? 
-                <a href="/register" className="font-bold text-white dark:text-white hover:underline"> Register here!</a>
+                You already have an account? 
+                <a href="/login" className="font-bold text-white dark:text-white hover:underline"> Login here!</a>
             </div>
-            <Button text="Login" />  
-            <Button text="Connect with Google" />  
+            <Button text="Register" />    
         </div>
     )
 }
