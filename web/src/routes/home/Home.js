ae72b0ca34027bb99657b2ec5cd84ff79040564a
Esteban <esteban.marques@epitech.eu>
2024-12-07 16:29:04 +0100

    feat[#48]: implement `HeaderBar` and `Button` component
    * Add `lucid` package

--- web/src/routes/home/Home.js
+++ web/src/routes/home/Home.js
@@ -5,6 +5,7 @@
 ** Home
 */
 
+import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
 
@@ -12,6 +13,7 @@ export default function Home() {
 
     return (
         <div>
+            <Header />
             <Content />
             <Footer />
         </div>
