de88a179cdf0391e950c551145c82d47c63848d6
Esteban <esteban.marques@epitech.eu>
2024-12-07 17:08:21 +0100

    feat[#48]: add background assets

--- web/src/routes/home/header/Header.js
+++ web/src/routes/home/header/Header.js
@@ -9,6 +9,8 @@ import Title from "./Title"
 import HeaderBar from "./HeaderBar"
 import Button from "../../../components/Button"
 
+import BeckgroundAssets from "../../../assets/effect.png"
+
 const dataRoutes = [
     {
         title: "Explore",
@@ -24,20 +26,38 @@ const dataRoutes = [
     }
 ]
 
+
 export default function Header() {
 
     return (
-        <div className="bg-gradient-to-l from-chartpurple-200 via-chartpurple-300 to-chartgray-200 mb-10">
+        <div className="relative bg-gradient-to-l from-chartpurple-200 via-chartpurple-300 to-chartgray-300 mb-10 overflow-hidden">
+            {/* Background Images */}
+            <div className="absolute inset-0 z-0">
+                <img
+                    src={BeckgroundAssets}
+                    alt="Background pattern"
+                    className="absolute top-[-300px] left-[-200px] w-[700px] rotate-[165deg]"
+                />
+                <img
+                    src={BeckgroundAssets}
+                    alt="Background wave"
+                    className="absolute top-[-400px] right-[-400px] w-[650px] rotate-[165deg]"
+                />
+            </div>
+    
+            {/* HeaderBar */}
             <HeaderBar routes={dataRoutes} />
-            <div className="flex flex-col items-center justify-center pb-5">
+
+            {/* Main Content */}
+            <div className="flex flex-col items-center justify-center pb-5 relative z-10">
                 <Title />
                 <Button
                     text={"Get started"}
                     redirect={true}
                     onClick={() => window.open("/register")}
-                    styleClolor={"bg-white text-chartgray-200 hover:bg-gray-200 text-xl"}
-                    />
+                    styleClolor={"bg-white text-chartgray-300 hover:bg-gray-200 text-xl"}
+                />
             </div>
         </div>
-    )
+    );
 }
