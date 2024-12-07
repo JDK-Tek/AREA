6388f1b1eccaef41e767209bd2e8e0b1345459f6
Esteban <esteban.marques@epitech.eu>
2024-12-07 16:29:04 +0100

    feat[#48]: implement `HeaderBar` and `Button` component
    * Add `lucid` package

---
+++ web/src/routes/home/HeaderBar.js
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2024
+** AREA
+** File description:
+** HeaderBar
+*/
+
+import Button from "../../components/Button";
+
+import Logo from './../../assets/fullLogo.png';
+
+export default function HeaderBar({ routes }) {
+    return (
+        <div className="flex justify-between items-center p-3">
+            <div>
+                <img src={Logo} alt="logo" className="h-[50px]"/>
+            </div>
+            <div className="flex flex-wrap justify-end gap-7 items-center">
+                {routes.map((route, index) => (
+                    <label
+                        key={index}
+                        className="font-bold font-spartan text-white p-5 text-lg cursor-pointer hover:text-gray-200"
+                        onClick={() => window.location.href = route.link}
+                    >{route.title}</label>
+                ))}
+                <Button
+                    text={"Get started"}
+                    redirect={false}
+                    onClick={() => window.location.href = "/register"}
+                    styleClolor={"bg-white text-chartgray-200 hover:bg-gray-200"}
+                />
+            </div>
+        </div>
+    );
+}
