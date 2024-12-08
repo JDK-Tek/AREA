cd9d6cc064c4a5d3432b149d339d07d8636cf728
John DE KETTELBUTTER <john.de-kettelbutter@epitech.eu>
2024-12-08 16:23:30 +0100

    refactor(#80): LoginRegisters components (box, textfields, button)

--- web/src/components/RegisterBox/RegisterBox.js
+++ web/src/components/RegisterBox/RegisterBox.js
@@ -8,9 +8,10 @@
 import React, { useState } from "react"
 import axios from "axios"
 
-import { LoginTextField } from "../LoginBox/LoginBox"
-import { LoginTextFieldsBox } from "../LoginBox/LoginBox"
-import { Button } from "../LoginBox/LoginBox"
+// Login/Register components
+import { LRTextFieldsBox } from "../TextFields/TextFields"
+import { LRButton } from "../Button"
+import LRBox from "../Box/Box"
 
 function RegisterTexts() {
     return (
@@ -61,16 +62,14 @@ export default function RegisterBox () {
     }
 
     return (
-        <div className="bg-gradient-to-b from-zinc-700 to-gray-800 flex flex-col justify-center 
-                        w-3/4 sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-2/3 
-                        h-4/6 sm:h-3/4 md:h-2/3 lg:h-3/4 rounded-md">
+        <LRBox>
             <RegisterTexts />
-            <LoginTextFieldsBox text1="Email" text2="Password" handleChangeField={handleChange}/>
+            <LRTextFieldsBox text1="Email" text2="Password" handleChangeField={handleChange}/>
             <div className="text-center pt-8 sm:pt-10 text-white text-sm sm:text-base md:text-lg">
                 You already have an account? 
                 <a href="/login" className="font-bold text-white dark:text-white hover:underline"> Login here!</a>
             </div>
-            <Button text="Register" handleClick={handleSubmit}/>    
-        </div>
+            <LRButton text="Register" handleClick={handleSubmit}/>    
+        </LRBox>
     )
 }
