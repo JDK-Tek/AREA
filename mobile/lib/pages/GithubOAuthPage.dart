c9c0993224fdff3f52fb9a395e53bf4128f5b21d
Elise PIPET <elise.pipet@epitech.eu>
2025-01-18 00:43:04 +0100

    fix(#103): redirect url oauth and connectivity check

--- mobile/lib/pages/GithubOAuthPage.dart
+++ mobile/lib/pages/GithubOAuthPage.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:area/pages/login_page.dart';
 import 'package:http/http.dart' as http;
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:go_router/go_router.dart';
@@ -9,31 +7,13 @@ import 'package:go_router/go_router.dart';
 class GithubLoginButton extends StatelessWidget {
   const GithubLoginButton({super.key});
 
-  Future<bool> _checkConnectivity() async {
-    var connectivityResult = await (Connectivity().checkConnectivity());
-
-    if (connectivityResult == ConnectivityResult.none) {
-      return false;
-    }
-    return true;
-  }
-
   Future<void> _launchURL(BuildContext context) async {
-    bool tmp = await _checkConnectivity();
-
-    if (!context.mounted) return;
-    if (!tmp) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => const LoginPage()),
-      );
-    } else {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => const GithubAuthPage()),
-      );
-    }
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const GithubAuthPage()),
+    );
   }
+
 
   @override
   Widget build(BuildContext context) {
