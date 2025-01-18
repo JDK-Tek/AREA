3b5d25b54c15909fb22097b1d33de8eabc1ed6da
Esteban <esteban.marques@epitech.eu>
2025-01-18 19:19:55 +0100

    feat[#174]: implement `new-art-sports` newsapi actions

--- server/services/newsapi/app.py
+++ server/services/newsapi/app.py
@@ -333,6 +333,44 @@ def new_article_health():
 
 	return jsonify({"status": "ok"}), 200
 
+ACTION_NEW_ART_SPORTS = "new-art-sports"
+oreo.create_area(
+	ACTION_NEW_ART_SPORTS,
+	NewOreo.TYPE_ACTIONS,
+	"When a new article is published in Sports category",
+	[]
+)
+@app.route(f'/{ACTION_NEW_ART_SPORTS}', methods=["POST"])
+def new_article_sports():
+	app.logger.info(f"{ACTION_NEW_ART_SPORTS} endpoint hit")
+
+	# get data
+	data = request.json
+	if not data:
+		return jsonify({"error": "Invalid JSON"}), 400
+
+	userid = data.get("userid")
+	bridge = data.get("bridge")
+	spices = data.get("spices", {})
+	if not userid or not bridge:
+		return jsonify({"error": f"Missing required fields: 'userid': {userid}, 'spices': {spices}, 'bridge': {bridge}"}), 400
+
+	with db.cursor() as cur:
+		cur.execute("INSERT INTO micro_newsapi" \
+			  "(userid, bridgeid, triggers, spices) " \
+			  "VALUES (%s, %s, %s, %s)", (
+				  userid,
+				  bridge,
+				  ACTION_NEW_ART_SPORTS,
+				  json.dumps(spices)
+			  )
+		)
+
+		db.commit()
+
+	return jsonify({"status": "ok"}), 200
+
+
 
 CATEGORIES = {
 	ACTION_NEW_ART_TECH: "technology",
