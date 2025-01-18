d4648800a47cdf6673241a4e7ca4aca3a456b1aa
Pol <114910509+paulogarithm@users.noreply.github.com>
2025-01-18 02:44:55 +0100

    Update roblox.sql

--- server/database/roblox.sql
+++ server/database/roblox.sql
@@ -3,7 +3,7 @@ drop database if exists micro_roblox;
 create table if not exists micro_roblox (
     id serial primary key,
     robloxid text not null,
-    gameid int not null,
+    gameid text not null,
     command text default null,
     constraint unique_gameid unique (gameid)
 );
