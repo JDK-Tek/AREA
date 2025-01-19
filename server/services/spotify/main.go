87c7e7d902d763e2ecf3e72db33c8bc0ce517bac
Gregoire lan tim <gregoire.lan-tim@epitech.eu>
2025-01-19 16:59:08 +0100

    feat(#102): genre for the action checkSong

--- server/services/spotify/main.go
+++ server/services/spotify/main.go
@@ -2824,7 +2824,6 @@ func addToPlaylistByName(w http.ResponseWriter, req *http.Request, db *sql.DB) {
 }
 
 
-
 func connectToDatabase() (*sql.DB, error) {
 	dbPassword := os.Getenv("DB_PASSWORD")
 	if dbPassword == "" {
