c55fbaf9f19e82aa60bdf14acb6f4a9375cbd62b
Paul PARISOT <paul.parisot@epitech.eu>
2025-01-19 21:27:16 +0100

    feat[#123]: implement full twitch oauth & action

--- server/services/openweather/app.py
+++ server/services/openweather/app.py
@@ -485,7 +485,7 @@ def webhook():
 	print("Starting newsapi webhook", file=sys.stderr)
 
 	while True:
-		print("Checking for new weather", file=sys.stderr)
+		# print("Checking for new weather", file=sys.stderr)
 		time.sleep(10)
 
 		with db.cursor() as cur:
@@ -493,7 +493,7 @@ def webhook():
 			   "WHERE triggers = %s", (ACTION_WEATHER_AT_CITY,))
 	
 			rows = cur.fetchall()
-			print(f"Checking weather for {len(rows)} users", file=sys.stderr)
+			# print(f"Checking weather for {len(rows)} users", file=sys.stderr)
 	
 			for row in rows:
 				userid, bridgeid, triggers, spices, last_weather = row
@@ -566,13 +566,13 @@ def webhook():
 					print(f"New weather detected: {weather} in {city}: {res.json()}", file=sys.stderr)
 
 			for (weather_data, path_comp) in WEATHER_DATA_COMP:
-				print(f"Checking data weather for {weather_data}", file=sys.stderr)
+				# print(f"Checking data weather for {weather_data}", file=sys.stderr)
 		
 				cur.execute("SELECT userid, bridgeid, triggers, spices, last_weather FROM micro_openweather "
 				   "WHERE triggers = %s", (weather_data,))
 		
 				rows = cur.fetchall()
-				print(f"Checking weather for {len(rows)} users", file=sys.stderr)
+				# print(f"Checking weather for {len(rows)} users", file=sys.stderr)
 		
 				for row in rows:
 					userid, bridgeid, triggers, spices, last_weather = row
