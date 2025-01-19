e75fb0a471a519cd79935ebcc5335941fd21a13e
Elise PIPET <elise.pipet@epitech.eu>
2025-01-19 17:28:48 +0100

    fix(#185): unused import

--- server/services/zoom/main.go
+++ server/services/zoom/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"bytes"
+	//"bytes"
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"io"
+	//"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
-	"time"
+	//"time"
 
-	"github.com/dgrijalva/jwt-go"
+	//"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
+	//_ "github.com/lib/pq"
 )
 
 const (
@@ -27,6 +27,28 @@ const (
 const PERMISSIONS = 8
 const EXPIRATION = 60 * 30
 
+var db *sql.DB
+
+type InfoSpice struct {
+	Name  string   `json:"name"`
+	Type  string   `json:"type"`
+	Title string   `json:"title"`
+	Extra []string `json:"extra"`
+}
+
+type InfoRoute struct {
+	Type   string      `json:"type"`
+	Name   string      `json:"name"`
+	Desc   string      `json:"description"`
+	Spices []InfoSpice `json:"spices"`
+}
+
+type Infos struct {
+	Color  string      `json:"color"`
+	Image  string      `json:"image"`
+	Routes []InfoRoute `json:"areas"`
+}
+
 func getUserInfo(w http.ResponseWriter, req *http.Request) {
 	authHeader := req.Header.Get("Authorization")
 	if authHeader == "" {
@@ -34,7 +56,7 @@ func getUserInfo(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	reqUser, err := http.NewRequest("GET", API_USER_SPOTIFY, nil)
+	reqUser, err := http.NewRequest("GET", ZOOM_API_ME_URL, nil)
 	if err != nil {
 		http.Error(w, "Erreur lors de la création de la requête utilisateur", http.StatusInternalServerError)
 		return
@@ -82,11 +104,87 @@ func getOAUTHLink(w http.ResponseWriter, req *http.Request) {
     fmt.Fprintln(w, str)
 }
 
-func main() {
-	db, err := connectToDatabase()
-	if err != nil {
-		os.Exit(84)
+func getRoutes(w http.ResponseWriter, req *http.Request) {
+	var list = []InfoRoute{
+		{
+			Name: "playMusic",
+			Type: "reaction",
+			Desc: "Chose a music to play !",
+			Spices: []InfoSpice{
+				{
+					Name:  "musique",
+					Type:  "text",
+					Title: "The message you want to send.",
+				},
+			},
+		},
+		{
+			Name: "pauseMusic",
+			Type: "reaction",
+			Desc: "Stop the current music !",
+			Spices: []InfoSpice{
+				{
+					Name:  "message",
+					Type:  "text",
+					Title: "The message you want to send.",
+				},
+			},
+		},
 	}
+	var infos = Infos{
+		Color:  "#1DB954",
+		Image:  "/assets/spotify.webp",
+		Routes: list,
+	}
+	var data []byte
+	var err error
+
+	data, err = json.Marshal(infos)
+	if err != nil {
+		http.Error(w, `{ "error":  "marshal" }`, http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, string(data))
+}
+
+func connectToDatabase() (*sql.DB, error) {
+	dbPassword := os.Getenv("DB_PASSWORD")
+	if dbPassword == "" {
+		log.Fatal("DB_PASSWORD not found")
+	}
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == "" {
+		log.Fatal("DB_USER not found")
+	}
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		log.Fatal("DB_HOST not found")
+	}
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		log.Fatal("DB_NAME not found")
+	}
+	dbPort := os.Getenv("DB_PORT")
+	if dbPort == "" {
+		log.Fatal("DB_PORT not found")
+	}
+	connectStr := fmt.Sprintf(
+		"postgresql://%s:%s@%s:%s/%s?sslmode=disable",
+		dbUser,
+		dbPassword,
+		dbHost,
+		dbPort,
+		dbName,
+	)
+	return sql.Open("postgres", connectStr)
+}
+
+func main() {
+	// db, err := connectToDatabase()
+	// if err != nil {
+	// 	os.Exit(84)
+	// }
 	fmt.Println("Zoom microservice container is running !")
 	router := mux.NewRouter()
 	godotenv.Load(".env")
